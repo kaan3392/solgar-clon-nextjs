d83a182e3e862194e95b362eff9913624e7ff6a4
kaan3392 <kaanalacali@gmail.com>
2022-06-01 13:57:37 +0300

    first

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  experimental: { styledComponents: true },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
