19666d87556e7271a92fd9218661f3fe5cafc8b2
kaan3392 <kaanalacali@gmail.com>
2022-06-07 13:44:01 +0300

    productProps

--- components/Types.tsx
+++ components/Types.tsx
@@ -1,12 +1,11 @@
+import { Document } from "mongoose";
+import { ReactNode } from "react";
+
 export interface DotProps {
   active?: boolean;
 }
 export interface ImageProps {
   active?: boolean;
-}
-
-export interface ArrowProps {
-    onClick: React.MouseEventHandler<HTMLDivElement>;
 }
 
 export interface ArrowDirectionProps {
@@ -15,4 +14,30 @@ export interface ArrowDirectionProps {
 }
 export interface HorizontalProps {
   horizontal?:boolean
+}
+export interface MenuProps {
+  menu?:boolean
+}
+
+export interface IProduct extends Document{
+  name: string ;
+  image: string;
+  category: string;
+  price: string;
+  titleEnglish?: string;
+  titleTurkish?: string;
+  short: string;
+  amount: string;
+  contentHeader: string;
+  contentKeys?: string[];
+  contentVals?: string[];
+  otherContents?: string;
+  howToUse: string;
+  extraInfo?: string;
+  images: string[];
+  children?:ReactNode;
+}
+
+export interface ProductsProps{
+  products: IProduct[];
 }
