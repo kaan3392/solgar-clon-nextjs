1927a036d5211bf25caaf441f93bb75057dfe9c5
kaan3392 <kaanalacali@gmail.com>
2022-06-07 13:42:19 +0300

    fixed

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -5,7 +5,14 @@ import styled from "styled-components";
 const Container = styled.div`
   height: 30px;
   background-color: #302519;
+  @media only screen and (max-width: 768px) {
+    position: fixed;
+    z-index: 100000;
+    top: 0;
+    width: 100%;
+  }
 `;
+
 const Wrapper = styled.div`
   padding: 0px 80px;
   height: 100%;
@@ -16,19 +23,20 @@ const Wrapper = styled.div`
     padding: 0 10px;
   }
 `;
+
 const Items = styled.div`
- display: flex;
- align-items: center;
- justify-content: flex-end;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
   height: 100%;
-  svg{
-      margin-left: 5px;
-      color: #FFE2B8;
-      cursor: pointer;
-      opacity: 0.6;
+  svg {
+    margin-left: 5px;
+    color: #ffe2b8;
+    cursor: pointer;
+    opacity: 0.6;
   }
   @media only screen and (max-width: 380px) {
-    svg{
+    svg {
       font-size: 20px;
     }
   }
