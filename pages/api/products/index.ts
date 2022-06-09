c1768dedf599f0324599bbe88d43d206c2bd8361
kaan3392 <kaanalacali@gmail.com>
2022-06-09 21:59:37 +0300

    .

--- pages/api/products/index.ts
+++ pages/api/products/index.ts
@@ -2,12 +2,13 @@ import { dbConnect } from "../../../util/mongo";
 import { IProduct } from "../../../components/Types";
 import Product from "../../../models/Product";
 import type { NextApiRequest, NextApiResponse } from "next";
+import {solgarData} from "../../../newSolgar"
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<IProduct[] | Error>
 ) {
-  dbConnect();
+  // dbConnect();
 
   if (req.method === "GET") {
     const { cat } = req.query;
@@ -28,7 +29,7 @@ export default async function handler(
         console.log("bbb")
       }
       console.log("ccc")
-      return res.status(200).json(products);
+      return res.status(200).json(solgarData as IProduct[]);
     } catch (error) {
       console.log("ddd")
       return res.status(500).json(error as Error);
