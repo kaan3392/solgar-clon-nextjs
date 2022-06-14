17ba8cdf20499c354930ab2d1275bc8d51c924b0
kaan3392 <kaanalacali@gmail.com>
2022-06-14 19:36:53 +0300

    .

--- pages/api/products/index.ts
+++ pages/api/products/index.ts
@@ -2,12 +2,21 @@ import { dbConnect } from "../../../util/mongo";
 import { IProduct } from "../../../components/Types";
 import Product from "../../../models/Product";
 import type { NextApiRequest, NextApiResponse } from "next";
+import NextCors from 'nextjs-cors';
+
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<IProduct[] | Error>
 ) {
   await dbConnect();
+
+  await NextCors(req, res, {
+    // Options
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+    origin: '*',
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+ });
 
   if (req.method === "GET") {
     const { cat } = req.query;
